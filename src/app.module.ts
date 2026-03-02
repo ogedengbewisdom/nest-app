10e49f3061a3805f5c1d43fa0dad122b1ded0431
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2026-03-02 12:57:10 +0100

    fix: folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
   imports: [ProductsModule],
