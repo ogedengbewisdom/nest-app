3e9c3909f4896b764411559d1a6fc6482d5b8235
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2026-04-22 06:58:15 +0100

    fix: health check in production

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './modules/auth/decorator/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): object {
     return this.appService.getHello();
+  }
+
+  @Public()
+  @Get('health')
+  healthCheck(): object {
+    return this.appService.healthCheck();
   }
 }
