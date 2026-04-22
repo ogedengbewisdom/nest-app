d3352f77eeadfd123a17e5df4178b4919a3d5f8a
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2026-04-22 12:10:33 +0100

    fix: get FE Url from env

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: process.env.FE_URL as string,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-user-email'],
     credentials: true,
